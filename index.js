05e513e5d49aa0442b9ea254a1d25fd323235193
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-07-06 10:19:36 +0530

    Payment made notif and total

--- index.js
+++ index.js
@@ -67,10 +67,17 @@ const resetResult = () => {
     `;
 }
 
-const addResult = (displayDate, principleAmount, interestAmount, penalAmount, className) => {
+const addResult = (displayDate, numOfDays, principleAmount, interestAmount, penalAmount, className) => {
+    if (className === 'paymentClass') {
+        document.getElementById('result').innerHTML += ` 
+            <h1>
+                Payment was made!
+            </h1>
+        `
+    }
     let entry = `
     <tr class=${className}>
-        <td>${displayDate.toLocaleDateString('en-GB')}</td>
+        <td>${displayDate.toLocaleDateString('en-GB')}, ${Math.round(numOfDays)}</td>
         <td>${seperator(Math.round((principleAmount + Number.EPSILON) * 100) / 100)}</td>
         <td>${seperator(Math.round((interestAmount + Number.EPSILON) * 100) / 100)}</td>
         <td>${seperator(Math.round((penalAmount + Number.EPSILON) * 100) / 100)}</td>
@@ -98,7 +105,7 @@ document.getElementById('submit').addEventListener('click', () => {
 
         //increment currentDate by 6 or 12 months (installment)
         beginDate = new Date(currentDate);
-        if (installmentNumber>0) { //add time by 6 mons
+        if (installmentNumber > 0) { //add time by 6 mons
             //console.log("Pay installment number", installmentNumber)
             currentDate.setMonth(currentDate.getMonth() + 6);
             installmentNumber -= 1;
@@ -113,7 +120,7 @@ document.getElementById('submit').addEventListener('click', () => {
 
         if (currentDate.getTime() > today) {
             //console.log("Time limit reached to today")
-            currentDate = new Date(today);
+            currentDate = new Date(new Date(today).toLocaleDateString());
             i = 100; // only find interest till today 
         }
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -126,7 +133,7 @@ document.getElementById('submit').addEventListener('click', () => {
             penalAmount = principleAmount * (penalInterest / 100) * numOfDays / 365;
 
             //append these values to result 
-            addResult(currentDate, principleAmount, interestAmount, penalAmount, null);
+            addResult(currentDate, numOfDays, principleAmount, interestAmount, penalAmount, null);
             principleAmount += interestAmount + penalAmount;
         } else {
             //else add interest but first check if payment was made
@@ -135,66 +142,68 @@ document.getElementById('submit').addEventListener('click', () => {
             if (paymentDate.getTime() <= currentDate.getTime()) {
                 //console.log("Payment was made before or on time")
                 let numOfDays = (paymentDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24);
-                
-                if (installmentNumber >=0) penalAmount = 0;
+
+                if (installmentNumber >= 0) penalAmount = 0;
                 else penalAmount = principleAmount * (penalInterest / 100) * numOfDays / 365;
 
                 interestAmount = principleAmount * (rateInterest / 100) * numOfDays / 365;
 
+                addResult(paymentDate, numOfDays, principleAmount, interestAmount, penalAmount, null);
+
                 //adjust the payment made in the principle and interest
                 let thisPaymentAmount = paymentHistory[currentPaymentNum][1];
-                console.log(installmentNumber, interestAmount, penalAmount, principleAmount)
-                if (thisPaymentAmount >= penalAmount){
+                // console.log(installmentNumber, interestAmount, penalAmount, principleAmount)
+                if (thisPaymentAmount >= penalAmount) {
                     thisPaymentAmount -= penalAmount;
-                    penalAmount=0;
-                }else{
+                    penalAmount = 0;
+                } else {
                     penalAmount -= thisPaymentAmount;
                     thisPaymentAmount = 0;
                 }
 
-                if (thisPaymentAmount >= interestAmount){
-                    thisPaymentAmount-=interestAmount; 
-                    interestAmount=0;
-                }else{
+                if (thisPaymentAmount >= interestAmount) {
+                    thisPaymentAmount -= interestAmount;
+                    interestAmount = 0;
+                } else {
                     interestAmount -= thisPaymentAmount;
                     thisPaymentAmount = 0;
                 }
                 principleAmount += penalAmount + interestAmount - thisPaymentAmount;
                 currentPaymentNum += 1;
-                addResult(paymentDate, principleAmount, interestAmount, penalAmount, 'paymentClass');
-                
+                addResult(paymentDate, numOfDays, principleAmount, interestAmount, penalAmount, 'paymentClass');
+
                 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 // interest from payment date to current date
                 numOfDays = (currentDate.getTime() - paymentDate.getTime()) / (1000 * 60 * 60 * 24);
-                if (installmentNumber >=0) penalAmount = 0;
+                if (installmentNumber >= 0) penalAmount = 0;
                 else penalAmount = principleAmount * (penalInterest / 100) * numOfDays / 365;
 
                 interestAmount = principleAmount * (rateInterest / 100) * numOfDays / 365;
-                addResult(currentDate, principleAmount, interestAmount, penalAmount, null);
+                addResult(currentDate, numOfDays, principleAmount, interestAmount, penalAmount, null);
 
                 principleAmount += interestAmount + penalAmount;
 
-            } else if (installmentNumber>=0 && (paymentDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24) <= 10) { // within 10 days 
+            } else if (installmentNumber >= 0 && (paymentDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60 * 24) <= 10) { // within 10 days 
                 //if made within 10 days, then no penalty and interest of 6 mos
                 //console.log("Payment was made within the warning time")
                 penalAmount = 0;
                 const numOfDays = (currentDate.getTime() - beginDate.getTime()) / (1000 * 60 * 60 * 24);
                 interestAmount = principleAmount * (rateInterest / 100) * numOfDays / 365;
-
+                addResult(currentDate, numOfDays, principleAmount, interestAmount, penalAmount, null);
                 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 //adjust the payment made in the principle and interest
                 let thisPaymentAmount = paymentHistory[currentPaymentNum][1];
-                if (thisPaymentAmount >= interestAmount){
-                    thisPaymentAmount-=interestAmount; 
-                    interestAmount=0;
-                }else{
+                if (thisPaymentAmount >= interestAmount) {
+                    thisPaymentAmount -= interestAmount;
+                    interestAmount = 0;
+                } else {
                     interestAmount -= thisPaymentAmount;
                 }
-                principleAmount += interestAmount -thisPaymentAmount;
+                principleAmount += interestAmount - thisPaymentAmount;
                 currentPaymentNum += 1;
 
                 //append these values to result 
-                addResult(currentDate, principleAmount, interestAmount, penalAmount, 'paymentClass');
+                addResult(paymentDate, numOfDays, principleAmount, interestAmount, penalAmount, 'paymentClass');
                 principleAmount += interestAmount + penalAmount;
 
             } else {
@@ -206,17 +215,23 @@ document.getElementById('submit').addEventListener('click', () => {
                 penalAmount = principleAmount * (penalInterest / 100) * numOfDays / 365;
 
                 //append these values to result 
-                addResult(currentDate, principleAmount, interestAmount, penalAmount, null);
+                addResult(currentDate, numOfDays, principleAmount, interestAmount, penalAmount, null);
                 principleAmount += interestAmount + penalAmount;
             }
 
         }
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        if (installmentNumber == 0){
+        if (installmentNumber == 0) {
             installmentNumber -= 1;
         }
-        
+
     }
+
+    document.getElementById('result').innerHTML += `
+        <h1>
+            Net outstanding dues = ${seperator(Math.round((principleAmount + interestAmount + penalAmount + Number.EPSILON) * 100) / 100)}
+        </h1>
+    `
 })
 
 
