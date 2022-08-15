32bb06a3edb95533c9f0552939373749baa5afe2
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-08-15 19:50:58 +0530

    Input data from modal

---
+++ client/src/Components/pages/AddAllottee.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { ModalContainer } from "../sections";
+import { TextField, MenuItem } from "@mui/material";
+
+export default function AddAllottee({
+  isOpen,
+  close,
+  username,
+  setUsername,
+  setAllotmentDate,
+  allotmentDate,
+  amountPrice,
+  setAmountPrice,
+  downPayment,
+  setDownPayment,
+  installmentsNumber,
+  setInstallmentsNumber,
+  rateInterest,
+  setRateInterest,
+  penalInterest,
+  setPenalInterest,
+  plot,
+  setPlot,
+}) {
+  return (
+    <ModalContainer isOpen={isOpen} close={close}>
+      <div className="formData">
+        <TextField
+          label="Username"
+          required={true}
+          variant="outlined"
+          type="text"
+          fullWidth
+          margin="normal"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)} //no space in username
+        />
+        <TextField
+          label="Date of Allotment"
+          variant="outlined"
+          type="Date"
+          fullWidth
+          margin="normal"
+          onChange={(e) => setAllotmentDate(e.target.value)}
+          value={allotmentDate}
+        />
+        <TextField
+          label="Amount Price"
+          required={true}
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={amountPrice}
+          onChange={(e) => setAmountPrice(e.target.value)}
+        />
+        <TextField
+          label="Down Payment (%)"
+          required={true}
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={downPayment}
+          onChange={(e) => setDownPayment(e.target.value)}
+        />
+        <TextField
+          label="Number of Installments"
+          required={true}
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={installmentsNumber}
+          onChange={(e) => setInstallmentsNumber(e.target.value)}
+        />
+        <TextField
+          label="Interest Rate"
+          required={true}
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={rateInterest}
+          onChange={(e) => setRateInterest(e.target.value)}
+        />
+        <TextField
+          label="Penality Rate"
+          required={true}
+          variant="outlined"
+          type="number"
+          fullWidth
+          margin="normal"
+          value={penalInterest}
+          onChange={(e) => setPenalInterest(e.target.value)}
+        />
+
+        <TextField
+          value={plot}
+          onChange={(e) => setPlot(e.target.value)}
+          select
+          fullWidth
+          margin="normal"
+          label="Type of plot"
+        >
+          <MenuItem key={1} value="Shop">
+            {" "}
+            Shop{" "}
+          </MenuItem>
+          <MenuItem key={2} value="Booth">
+            {" "}
+            Booth{" "}
+          </MenuItem>
+        </TextField>
+      </div>
+    </ModalContainer>
+  );
+}
---
+++ client/src/Components/pages/AddPayment.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { ModalContainer } from "../sections";
+import { Button, TextField } from "@mui/material";
+
+export default function AddPayment({
+  isOpen,
+  close,
+  paymentsHistory,
+  setPaymentsHistory,
+}) {
+  const [paymentDate, setPaymentDate] = useState(new Date());
+  const [paymentAmount, setPaymentAmount] = useState(0);
+
+  const addPayment = () => {
+    setPaymentsHistory([
+      ...paymentsHistory,
+      {
+        id: paymentsHistory.length,
+        paymentDate,
+        paymentAmount,
+      },
+    ]);
+  };
+
+  return (
+    <ModalContainer isOpen={isOpen} close={close}>
+      <h1>Payment {paymentsHistory.length + 1}</h1>
+
+      <TextField
+        type="date"
+        InputLabelProps={{ shrink: true }}
+        label="Payment Date"
+        margin="normal"
+        value={paymentDate}
+        onChange={(e) => {
+          setPaymentDate(e.target.value);
+        }}
+      />
+
+      <TextField
+        type="number"
+        InputLabelProps={{ shrink: true }}
+        label="Payment Amount"
+        margin="normal"
+        value={paymentAmount}
+        onChange={(e) => {
+          setPaymentAmount(e.target.value);
+        }}
+      />
+
+      <div className="buttonContainer">
+        <Button variant="contained" color="secondary" onClick={addPayment}>
+          Add Payment
+        </Button>
+      </div>
+    </ModalContainer>
+  );
+}
